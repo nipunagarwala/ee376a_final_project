e4aa86070da0ff47b376b88e262f44eef583e4bf
Nipun Agarwala <nipun.agarwala@stanford.edu>
2017-03-22 23:21:36 -0700

    Got AMP working

--- algo_by_nipun.py
+++ algo_by_nipun.py
@@ -1,44 +1,65 @@
 import numpy as np 
 import os
 import sys
+from algorithms import Algorithm
 
 
 
 
 class AMP(Algorithm):
 
-	def __init__(self, input_func, input_func_args, A_func, N, M, epsilon=1e-4, num_iter = 2000):
+	def __init__(self, input_func, input_func_args, A_func, N, M, epsilon=1e-2, lam = 2, num_iter = 2000):
 		Algorithm.__init__(self, input_func, input_func_args, A_func, N, M)
 		self.epsilon = epsilon
 		self.num_iter = 1000
+		self.lam = lam
 
 
 	def predict(self):
-		print self.A
 
 		def eta(gamma, tau):
+			res = np.zeros_like(gamma)
+			# for i in range(0, len(gamma)):
+			# 	if gamma[i] > tau:
+			# 		res[i] = gamma[i] - tau
+			# 	elif gamma[i] < -tau:
+			# 		res[i] = gamma[i] + tau
+
+			# return res
 			return np.sign(gamma)*np.maximum(np.abs(gamma) - tau, np.zeros_like(gamma))
 
 		def eta_prime(gamma, tau):
-			return (gamma > tau) + (gamma < tau)
+			return ((gamma > tau) + (gamma < -tau)).astype(int)
 
 		n,N = self.A.shape
-
-		xhat = zeros(N, 1);
-		z = y
-
+		k = 10
+		xhat = np.zeros((N, 1))
+		# z = np.expand_dims(self.y, axis=1)
+		z = np.expand_dims(np.zeros_like(self.y), axis=1)
+		cur_y = np.expand_dims(self.y, axis=1)
+		thresh = 1.
 		for i in range(0, self.num_iter):
-			gamma = xhat + np.dot(A.T, z)
+			xold = xhat
+			thresh_old = thresh
 
-			threshold = (np.sort(abs(gamma))[::-1])[n]
+			r = xhat + np.dot(self.A.T, z)
+			xhat  = eta(r, thresh)
 
-			xhat  = eta(gamma, threshold)
+			# thresh = np.sqrt(np.sum((xhat)**2)/N)*self.lam
+			thresh = thresh*1.0/n*np.mean(eta_prime(r, thresh))
 
-			print "Error is: {0}".format(np.mean((x-xhat)**2))
 
-			z = y - np.dot(A,xhat) + (z/n)*np.sum(eta_prime(gamma, threshold))
+			print "Error is: {0}".format(np.mean((self.x-xhat)**2))
+			# print "L2 Norm of x: {0}".format(np.sum(self.x**2))
+			# print "L2 Norm of xhat: {0}".format(np.sum(xhat**2))
 
-			if np.sum((y - np.dot(A,xhat))**2)/np.sum(y**2) < self.epsilon:
+			# print "Checking random norms: {0}".format(cur_y - np.dot(self.A,xhat))
+
+			z = cur_y - np.dot(self.A,xhat) + (z*1.0/n)*np.mean(eta_prime(xold + np.dot(self.A.T, z), thresh_old))
+
+			print "Done with iteration {0} \n".format(i)
+			if np.sum((cur_y - np.dot(self.A,xhat))**2)/np.sum(cur_y**2) < self.epsilon:
+				self.x_pred = np.round(xhat)
 				print "Our estimate xhat is: {0}".format(xhat)
 				break
 
